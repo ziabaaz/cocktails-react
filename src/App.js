d26ee0540abe9456a11bf28a55f9aa46f8702d5c
Rahman,Zia Ul <ziaul.rahman@honeywell.com>
2020-04-29 15:35:20 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import CocktailsList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("a");
+  const [cocktails, setCocktails] = useState([]);
+
+  useEffect(() => {
+    async function getDrinks() {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        );
+        const data = await response.json();
+        const { drinks } = data;
+        if (drinks) {
+          const newCocktails = drinks.map((item) => {
+            const {
+              idDrink,
+              strDrink,
+              strDrinkThumb,
+              strAlcoholic,
+              strGlass,
+            } = item;
+            return {
+              id: idDrink,
+              name: strDrink,
+              image: strDrinkThumb,
+              info: strAlcoholic,
+              glass: strGlass,
+            };
+          });
+          setCocktails(newCocktails);
+        } else {
+          setCocktails([]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+    getDrinks();
+  }, [searchTerm]);
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailsList cocktails={cocktails} loading={loading} />
+    </main>
+  );
+}
